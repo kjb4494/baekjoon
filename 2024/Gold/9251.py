69cf2260e4acc07256065c36050eb5867c14e0fb
khsFirefox <rlagnltn135@smtown.com>
2024-01-13 22:18:27 +0900

    1916, 9251

--- 2024/Gold/9251.py
+++ 2024/Gold/9251.py
@@ -15,5 +15,4 @@ if __name__ == '__main__':
                 dp[i][j] = dp[i-1][j-1] + 1
             else:
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-    print(dp)
     print(dp[-1][-1])
